2872eb8ea2c980991b0c528d7d5a7e07bbb95fac
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-04-04 23:02:52 +0100

    tidy up.. cross-browser transitions.. movement colours..

--- web/app.js
+++ web/app.js
@@ -4,22 +4,31 @@ var league = new window.league.League()
     .addTeam(window.league._teams)
     .addResult(window.league._games);
 
-var table = document.getElementById("table");
-
-var teamElements = [];
-league.history[0].table.forEach(function(team){
-    var teamElement = buildRow(team);
-    table.appendChild(teamElement.row);
-    teamElements[team.id] = teamElement;
-});
-
-var i=1;
+var table = document.getElementById("table"),
+    date = document.getElementById("date"),
+    teamElements = [],
+    i=0;
+    
 function updateTable(){
     var history = league.history[i++];
     if (history){
+        date.textContent = new Date(history.date).toDateString();
         history.table.forEach(function(team){
             var teamElement = teamElements[team.id];
-            teamElement.row.style.top = (team.position - 1) + "em";
+            if (!teamElement){
+                teamElement = buildRow(team);
+                table.appendChild(teamElement.row);
+                teamElements[team.id] = teamElement;
+            }
+            
+            teamElement.row.style.top = ((team.position-1) * 28) + "px";
+            if (team.move == "up"){
+                teamElement.row.className = "team up";
+            } else if (team.move == "down"){
+                teamElement.row.className = "team down";
+            } else {
+                teamElement.row.className = "team";
+            }
             teamElement.played.textContent = team.played;
             teamElement.won.textContent = team.won;
             teamElement.drawn.textContent = team.drawn;
@@ -34,7 +43,8 @@ function updateTable(){
     }
 }
 
-var updateInterval = setInterval(updateTable, 2000);
+updateTable();
+var updateInterval = setInterval(updateTable, 1600);
 
 function buildRow(team){
     var teamElement = {
@@ -71,7 +81,6 @@ function buildRow(team){
     teamElement.row.appendChild(teamElement.points);
     teamElement.row.className = "team";
     teamElement.row.id = team.team;
-    teamElement.row.style.top = (team.position - 1) + "em";
     return teamElement;
 }
 
